16f8e346497b8f5de6be44526437076ae5ec59cd
Tamer Mansour <78074218+Tamer-Mansour@users.noreply.github.com>
2022-08-26 16:38:52 +0300

    SHOP-6: error while display error alert

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -3,6 +3,8 @@ import {Col, Row} from "react-bootstrap";
 import Product from "../components/Porduct/Product";
 import {useDispatch, useSelector} from "react-redux";
 import {listProducts} from "../actions/productActions";
+import Loader from "../components/loader/Loader";
+import Message from "../components/message/Message";
 
 const HomeScreen = () => {
     const dispatch = useDispatch();
@@ -16,27 +18,17 @@ const HomeScreen = () => {
         loadProducts();
     }, [dispatch]);
 // const products = []
-    return (
-        <>
-            <h1>Latest Products</h1>
-            {loading
-                ? <h2>Loading...</h2>
-                : error
-                    ? <h3>{error}</h3>
-                    : <Row>
-                        {products.length > 0 &&
-                            products.map(product => {
-                                return (
-                                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                                        <Product product={product}/>
-                                    </Col>
-                                );
-                            })}
-                    </Row>
-            }
+    return (<>
+        <h1>Latest Products</h1>
+        {loading ? <Loader/> : error ? <h1>{error}</h1>: <Row>
+            {products.length > 0 && products.map(product => {
+                return (<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                    <Product product={product}/>
+                </Col>);
+            })}
+        </Row>}
 
-        </>
-    );
+    </>);
 }
 
 export default HomeScreen;
--- server/base/models.py
+++ server/base/models.py
@@ -14,7 +14,7 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     rating = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     numReviews = models.IntegerField(null=True, blank=True, default=0)
-    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    price = models.DecimalField(max_digits=12, decimal_places=2, null=True, blank=True)
     countInStock = models.IntegerField(null=True, blank=True, default=0)
     createdAt = models.DateTimeField(auto_now_add=True, editable=False)
 
