b6c0e4b02924f960c8e8ab7aad10533d5b498ba5
Tamer Mansour <78074218+Tamer-Mansour@users.noreply.github.com>
2022-08-29 15:22:43 +0300

    SHOP-7: create cart page

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,12 +2,22 @@ import {createStore, combineReducers, applyMiddleware} from "redux";
 import thunk from "redux-thunk";
 import {composeWithDevTools} from "redux-devtools-extension";
 import {productDetailsReducer, productListReducer} from "../reducers/productReducers";
+import {cartReducers} from "../reducers/cartReducers";
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    cart: cartReducers,
 });
-const initialState = {}
+
+const cartItemsFromStorage = localStorage.getItem('cartItems') ?
+    JSON.parse(localStorage.getItem('cartItems')) : []
+
+const initialState = {
+    cart: {
+        cartItems: cartItemsFromStorage,
+    }
+}
 const middleware = [thunk]
 const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
 
