54cf6937c795f3749fde4e4c1bf1f105cf8f1d33
Tamer Mansour <78074218+Tamer-Mansour@users.noreply.github.com>
2022-08-31 15:07:48 +0300

    SHOP-9: create redux for login

---
+++ client/src/actions/userActions.js
@@ -0,0 +1,33 @@
+import {USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS} from "../constants/userConstants";
+import axios from "axios";
+
+
+export const login = (email, password) => async (dispatch) => {
+    try {
+        dispatch({
+            type: USER_LOGIN_REQUEST
+        })
+        const config = {
+            headers:{
+                'content-type':'application/json'
+            }
+        }
+        const {data} = await axios.post(`api/users/login/`,
+            {'username': email, 'password': password})
+
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data
+        });
+
+        localStorage.setItem('userInfo', JSON.stringify(data))
+
+    } catch (error) {
+        dispatch({
+            type: USER_LOGIN_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message,
+        });
+    }
+}
