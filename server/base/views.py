705d639e2cd02b1b93695d9352b89cf14d7a3cdd
Tamer Mansour <78074218+Tamer-Mansour@users.noreply.github.com>
2022-08-31 13:22:23 +0300

    SHOP-8: access to admin and to non admin

--- server/base/views.py
+++ server/base/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from django.contrib.auth.models import User
 
 from .models import Product
 from .serializer import ProductSerializer, UserSerializer, UserSerializerWithToken
@@ -40,6 +42,7 @@ def getRoutes(request):
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
@@ -47,6 +50,15 @@ def getUserProfile(request):
 
 
 @api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getProducts(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
